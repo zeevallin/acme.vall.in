e2e81abeb3cd3820338f585e444b06bcfcb835af
zeeraw <zee@vall.in>
2019-01-18 14:05:24 +0100

    fix bug in docker run script

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 ACME_PATH=$1
-docker build $ACME_PATH -t acmedns
-docker run $ACME_PATH --rm --name acmedns \
+docker build $ACME_PATH -t zeeraw/acme.vall.in:latest
+docker run --rm --name acmedns \
     -p 53:53 \
     -p 53:53/udp \
     -p 80:80 \
     -v $ACME_PATH/config:/etc/acme-dns:ro \
-    -v $ACME_PATH/data:/var/lib/acme-dns  \
-    -v $ACME_PATH/certs:/var/certs  \
-    -d acmedns
+    -v $ACME_PATH/data:/var/lib/acme-dns \
+    -v $ACME_PATH/certs:/var/certs \
+    -d zeeraw/acme.vall.in:latest
